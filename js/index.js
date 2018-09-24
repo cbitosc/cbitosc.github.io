9e9e957a11f7e2400f8422ae52da3e6134a9967d
Ram Damera <ramdamera@outlook.com>
2018-09-25 00:40:05 +0530

    Add images, little content
    Iam sceptical about the hero background

--- js/index.js
+++ js/index.js
@@ -1,114 +1,122 @@
-'use strict';
+(function() {
+  'use strict';
 
-var banner = document.querySelector('.hero');
+  var banner = document.querySelector('.hero');
 
-var group1 = banner.querySelector('.hero__level_1');
-var group2 = banner.querySelector('.hero__level_2');
-var group3 = banner.querySelector('.hero__level_3');
+  var group1 = banner.querySelector('.hero__level_1');
+  var group2 = banner.querySelector('.hero__level_2');
+  var group3 = banner.querySelector('.hero__level_3');
 
-var orientation = {
-  beta: 0,
-  gamma: 0,
-};
-
-var mousePos = {
-  x: 0,
-  y: 0,
-};
-
-function debounce(func, wait, immediate) {
-  var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
+  var orientation = {
+    beta: 0,
+    gamma: 0,
   };
-};
 
-function onMouseMove(e) {
-  var clientX = e.clientX;
-  var clientY = e.clientY;
+  var mousePos = {
+    x: 0,
+    y: 0,
+  };
 
-  console.log(clientX, clientY);
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
 
-  mousePos.x = clientX;
-  mousePos.y = clientY;
+  function onMouseMove(e) {
+    var clientX = e.clientX;
+    var clientY = e.clientY;
 
-  var windowWidth = window.innerWidth;
-  var windowHeight = window.innerHeight;
+    console.log(clientX, clientY);
 
-  var xPivot = windowWidth / 2;
-  var yPivot = windowHeight / 2;
+    mousePos.x = clientX;
+    mousePos.y = clientY;
 
-  // don't judge my math
-  orientation.beta = (xPivot - clientX) / 100;
-  orientation.gamma = (yPivot - clientY) / 100;
-};
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
 
-function animationLoop() {
-  applyTransforms(orientation.beta, orientation.gamma);
+    var xPivot = windowWidth / 2;
+    var yPivot = windowHeight / 2;
 
-  requestAnimationFrame(animationLoop);
-};
+    // don't judge my math
+    orientation.beta = (xPivot - clientX) / 100;
+    orientation.gamma = (yPivot - clientY) / 100;
+  };
 
-var portrait = window.innerWidth < window.innerHeight;
-
-function handleOrientation(evt) {
-  var beta = evt.beta % 360;
-  var gamma = evt.gamma % 360;
-  orientation.beta = beta;
-  orientation.gamma = gamma;
-
-  if (portrait) {
-    if (beta >= 0 && beta <= 90) {
-      orientation.beta = (beta - 45) / 90 * 100;
-    } else {
-      beta = Math.abs(beta) % 90;
-      orientation.beta = beta / 90 * 100;
-    }
-  } else {
-    if (gamma >= 0 && gamma <= 90) {
-      orientation.gamma = (gamma - 45) / 90 * 100;
-    } else {
-      gamma = Math.abs(gamma) % 90;
-      orientation.gamma = gamma / 90 * 100;
-    }
-  }
-
-  if (portrait) {
+  function animationLoop() {
     applyTransforms(orientation.beta, orientation.gamma);
-  } else {
-    applyTransforms(orientation.gamma, orientation.beta);
+
+    requestAnimationFrame(animationLoop);
+  };
+
+  var portrait = window.innerWidth < window.innerHeight;
+
+  function handleOrientation(evt) {
+    var beta = evt.beta % 360;
+    var gamma = evt.gamma % 360;
+    orientation.beta = beta;
+    orientation.gamma = gamma;
+
+    if (portrait) {
+      if (beta >= 0 && beta <= 90) {
+        orientation.beta = (beta - 45) / 90 * 100;
+      } else {
+        beta = Math.abs(beta) % 90;
+        orientation.beta = beta / 90 * 100;
+      }
+    } else {
+      if (gamma >= 0 && gamma <= 90) {
+        orientation.gamma = (gamma - 45) / 90 * 100;
+      } else {
+        gamma = Math.abs(gamma) % 90;
+        orientation.gamma = gamma / 90 * 100;
+      }
+    }
+
+    if (portrait) {
+      applyTransforms(orientation.beta, orientation.gamma);
+    } else {
+      applyTransforms(orientation.gamma, orientation.beta);
+    }
+  };
+
+  function applyTransforms(xPercent, yPercent) {
+    // Moves a lot, contains specs
+    group1.style.transform = 'translate(' + xPercent + '%, ' + yPercent + '%)';
+
+    // Moves a little less, contains machinery
+    group2.style.transform = 'translate(' + xPercent / 1.25 + '%, ' + yPercent / 1.25 + '%)';
+
+    // Moves a lot less, contains text and art
+    group3.style.transform = 'translate(' + xPercent / 1.5 + '%, ' + yPercent / 1.5 + '%)';
+  };
+
+  banner.addEventListener('mousemove', debounce(onMouseMove, 16));
+
+  window.addEventListener('resize', debounce(function() {
+    onMouseMove({clientX: window.innerWidth/2, clientY: window.innerHeight/2});
+  }, 350));
+
+
+  if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', handleOrientation);
   }
-};
 
-function applyTransforms(xPercent, yPercent) {
-  // Moves a lot, contains specs
-  group1.style.transform = 'translate(' + xPercent + '%, ' + yPercent + '%)';
 
-  // Moves a little less, contains machinery
-  group2.style.transform = 'translate(' + xPercent / 1.25 + '%, ' + yPercent / 1.25 + '%)';
+  // call animation loop
+  animationLoop();
 
-  // Moves a lot less, contains text and art
-  group3.style.transform = 'translate(' + xPercent / 1.5 + '%, ' + yPercent / 1.5 + '%)';
-};
-
-banner.addEventListener('mousemove', debounce(onMouseMove, 16));
-
-if (window.DeviceOrientationEvent) {
-  window.addEventListener('deviceorientation', handleOrientation);
-}
-
-// call animation loop
-animationLoop();
-
-window.addEventListener('load', function() {
-  // initially move to center
-  onMouseMove({clientX: window.innerWidth/2, clientY: window.innerHeight/2});
-});
+  window.addEventListener('load', function() {
+    // initially move to center
+    onMouseMove({clientX: window.innerWidth/2, clientY: window.innerHeight/2});
+  });
+})();
