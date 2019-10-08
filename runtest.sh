3a82470d658feb85ab9df9a9a10f13282630ca21
Ram <37770235+r0mflip@users.noreply.github.com>
2019-10-08 09:21:36 +0530

    ci: take 2 on fixing ci (#11)
    * ci: fix-ci
    
    * chore: make checklog.js executable

--- runtest.sh
+++ runtest.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-# remove old logs
-rm -f lighthouse-log.json
+$CHROME_PATH --version
+
+# remove logs
+rm -f *-log.json
 
 # run server and get its pid
-serve -l 5858 &
+serv -p 5858 -d . >/dev/null &
 p="$!"
 
 # quit if server fails to run
 if [[ -z "$p" ]]; then
   echo "> Failed to start server"
   exit 1
+else
+  echo "> Server started"
 fi
 
 # run lighthouse test
-lighthouse --output=json --output-path=./lighthouse-log.json http://127.0.0.1:5858/
+lighthouse \
+  --output=json --output-path=./lighthouse-log.json \
+  --only-categories=accessibility,best-practices,performance,seo \
+  --quiet --chrome-flags="--headless" \
+  http://127.0.0.1:5858/
 
 if [[ $? -gt 0 ]]; then
   echo "> Lighthouse failed to run"
@@ -22,7 +30,8 @@ if [[ $? -gt 0 ]]; then
 fi
 
 # check values
-node ./checklog.js ./lighthouse-log.json
+echo "> Checking logs"
+./checklog.js ./lighthouse-log.json
 
 # kill the server
 kill $p
