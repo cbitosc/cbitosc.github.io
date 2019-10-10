3819650af27eb2c97b4928969b9cfb62f04f7ee0
cesar970 <cesar_augusto970@hotmail.com>
2019-10-09 21:19:48 -0300

    I did the lazy load using JS, is more compatible with browsers

--- js/index.js
+++ js/index.js
@@ -87,3 +87,25 @@
     onMouseMove({clientX: window.innerWidth/2, clientY: window.innerHeight/2});
   });
 })();
+
+//Lazy load
+let images = [...document.querySelectorAll('.lazy-image')];
+
+const interactSettings = {
+  root: document.querySelector('.center'),
+  rootMargin: '0px 0px 200px 0px'
+};
+
+function onIntersection(imageEntites) {
+  imageEntites.forEach(image => {
+    if (image.isIntersecting) {
+      observer.unobserve(image.target);
+      image.target.src = image.target.dataset.src;
+      image.target.onload = () => image.target.classList.add('loaded')
+    }
+  })
+}
+
+let observer = new IntersectionObserver(onIntersection, interactSettings);
+
+images.forEach(image => observer.observe(image));
